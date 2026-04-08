3abfab18e7a93c2a0da9a371ff1cea00093a2225
akari <jvjdev@gmail.com>
2026-04-08 15:13:19 +0800

    add models

--- apps/substore-server/cmd/substore-server/main.go
+++ apps/substore-server/cmd/substore-server/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-"net/http"
+	"encoding/json"
+	"net/http"
+
+	substoreserver "github.com/jvj-wonderland/substore/server"
 )
 
 func main() {
@@ -9,4 +12,43 @@ func main() {
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 
 	})
+
+	apiMux := http.NewServeMux()
+	apiMux.HandleFunc("POST /sources", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	apiMux.HandleFunc("GET /sources", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	mux.Handle("/api/", http.StripPrefix("/api", apiMux))
+
+	http.ListenAndServe(":8080", mux)
+}
+
+type AddSubscriptionSourceRequest struct {
+	Type    string          `json:"type"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+type SubscriptionSourcePayload struct {
+	Name string   `json:"name"`
+	Tags []string `json:"tags"`
+}
+
+type RemoteSubscriptionSourcePayload struct {
+	SubscriptionSourcePayload
+	FetchMode      substoreserver.FetchMode `json:"fetch_mode"`
+	UpdateInterval int64                    `json:"update_interval"`
+}
+
+type LocalSubscriptionSourcePayload struct {
+	SubscriptionSourcePayload
+	Content string `json:"content"`
+}
+
+type AddSubscriptionSinkRequest struct {
+	Name           string                    `json:"name"`
+	SinkFormat     substoreserver.SinkFormat `json:"sink_format"`
+	PipelineScript string                    `json:"pipeline_script"`
 }
---
+++ apps/substore-server/models.go
@@ -0,0 +1,52 @@
+package substoreserver
+
+import "fmt"
+
+type FetchMode int
+
+const (
+	FetchModeServer FetchMode = iota
+	FetchModeBrowser
+)
+
+type SinkFormat int
+
+const (
+	SinkFormatJSON SinkFormat = iota
+	SinkFormatYAML
+)
+
+func (fm FetchMode) String() string {
+	switch fm {
+	case FetchModeServer:
+		return "server"
+	case FetchModeBrowser:
+		return "browser"
+	default:
+		panic(fmt.Errorf("invalid FetchMode: %d", fm))
+	}
+}
+
+type SubscriptionSource struct {
+	ID   string
+	Name string
+	Tags []string
+}
+
+type RemoteSubscriptionSource struct {
+	SubscriptionSource
+	FetchMode      FetchMode
+	UpdateInterval int64
+	LastUpdated    int64
+	Content        *string
+}
+
+type LocalSubscriptionSource struct {
+	SubscriptionSource
+	Content string
+}
+
+type SubscriptionSink struct {
+	Name           string
+	PipelineScript string
+}
